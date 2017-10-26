652c35fe669fab382ba107e36728cb12d8263f5b
Tom Clark <thc29@MacBook-Pro-2.local>
2017-10-26 14:11:00 +0100

    Updated the plotlyoffline function to return html which actually renders, and to auto-download the plotly offline bundle if it isnt present.

--- plotly/plotly_offline_aux/plotlyoffline.m
+++ plotly/plotly_offline_aux/plotlyoffline.m
@@ -12,18 +12,21 @@ function response = plotlyoffline(plotlyfig)
         bundle_name = 'plotly-matlab-offline-bundle.js';
         bundle_file = fullfile(plotly_js_folder, bundle_name);
 
-        % check that the bundle exists
-        try
-            bundle = fileread(bundle_file);
-            % template dependencies
-            dep_script = sprintf('<script type="text/javascript">%s</script>\n', ...
-                bundle);
-        catch
-            error(['Error reading: %s.\nPlease download the required ', ...
-                   'dependencies using: >>getplotlyoffline \n', ...
-                   'or contact support@plot.ly for assistance.'], ...
-                   bundle_file);
+        % Check that the bundle exists, attempt to download if not
+        if ~exist(bundle_file, 'file')
+            %TODO move bundle_url to plotly config
+            bundle_url = 'http://cdn.plot.ly/plotly-latest.min.js';
+            warning('No plotly offline file: %s.\nAttempting to download required dependency from %s',...
+                    bundle_file,...
+                    bundle_url)
+            bundle_dir = fileparts(bundle_file);
+            mkdir(bundle_dir);
+            websave(bundle_file, bundle_url);
         end
+        
+        % template dependencies
+        dep_script = sprintf('<script src="%s"></script>\n', bundle_file);
+        
     else
         dep_script = '';
     end
@@ -44,8 +47,8 @@ function response = plotlyoffline(plotlyfig)
     jlayout = m2json(plotlyfig.layout);
     clean_jdata = escapechars(jdata); 
     clean_jlayout = escapechars(jlayout);
-                     
-    % template environment vars        
+    
+    % template environment vars
     plotly_domain = plotlyfig.UserData.PlotlyDomain;
     env_script = sprintf(['<script type="text/javascript">', ...
                           'window.PLOTLYENV=window.PLOTLYENV || {};', ...
@@ -53,7 +56,7 @@ function response = plotlyoffline(plotlyfig)
                           'Plotly.LINKTEXT="%s";', ...
                           '</script>'], plotly_domain, link_text); 
         
-    % template Plotly.plot
+    % template Plotly.plot   
     script = sprintf(['\n Plotly.plot("%s", %s, %s).then(function(){'...
                       '\n    $(".%s.loading").remove();' ...
                       '\n    $(".link--embedview").text("%s");'...
@@ -61,15 +64,18 @@ function response = plotlyoffline(plotlyfig)
                       id, link_text);
     
     plotly_script = sprintf(['\n<div class="%s loading" style=', ...
-                             'color: rgb(50,50,50);">Drawing...</div>' ... 
+                             'color: rgb(50,50,50);"></div>' ... << TODO insert 'Drawing...' as text in this div if jquery available
                              '\n<div id="%s" style="height: %s;',...
                              'width: %s;" class="plotly-graph-div">' ...
                              '</div> \n<script type="text/javascript">' ...
                              '%s \n</script>'], id, id, height, width, ... 
                              script);
     
-    % template entire script
-    offline_script = [dep_script env_script plotly_script]; 
+    % The plot function requires jquery (slim, min, only) as a dependency 
+    % to use the $() operator. If it's not there, then the 'Drawing' text
+    % and embeddable link don't get updated on plot... temporarily removed
+    offline_script = sprintf('<head>\n%s\n%s\n</head>\n<body>\n%s\n%s\n</body>', dep_script, env_script, plotly_script);
+
     filename = plotlyfig.PlotOptions.FileName; 
     
     % remove the whitespace from the filename
